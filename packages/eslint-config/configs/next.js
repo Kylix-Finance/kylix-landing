90c0596c70d43b19e9cbe54ce3d1f917d7311015
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-07-06 23:27:23 +0330

    feat: config tailwindcss

--- packages/eslint-config/configs/next.js
+++ packages/eslint-config/configs/next.js
@@ -3,38 +3,38 @@ const { defineConfig } = require("eslint-define-config");
 const baseConfig = require("./base.js");
 
 const nextConfig = defineConfig({
-	...baseConfig,
-	extends: [
-		...baseConfig.extends,
-		"eslint-config-turbo",
-		"next",
-		"next/core-web-vitals",
-		"plugin:react/jsx-runtime",
-		"plugin:react/recommended",
-		require.resolve("@vercel/style-guide/eslint/next"),
-	],
-	globals: {
-		...baseConfig.globals,
-		JSX: true,
-		React: true,
-	},
-	parserOptions: {
-		...baseConfig.parserOptions,
-		ecmaFeatures: {
-			...baseConfig.parserOptions.ecmaFeatures,
-			jsx: true,
-		},
-	},
-	plugins: [...baseConfig.plugins, "react"],
-	rules: {
-		...baseConfig.rules,
-	},
-	settings: {
-		...baseConfig.settings,
-		react: {
-			version: "detect",
-		},
-	},
+  ...baseConfig,
+  extends: [
+    ...baseConfig.extends,
+    "turbo",
+    "next",
+    "next/core-web-vitals",
+    "plugin:react/jsx-runtime",
+    "plugin:react/recommended",
+    require.resolve("@vercel/style-guide/eslint/next"),
+  ],
+  globals: {
+    ...baseConfig.globals,
+    JSX: true,
+    React: true,
+  },
+  parserOptions: {
+    ...baseConfig.parserOptions,
+    ecmaFeatures: {
+      ...baseConfig.parserOptions.ecmaFeatures,
+      jsx: true,
+    },
+  },
+  plugins: [...baseConfig.plugins, "react"],
+  rules: {
+    ...baseConfig.rules,
+  },
+  settings: {
+    ...baseConfig.settings,
+    react: {
+      version: "detect",
+    },
+  },
 });
 
 module.exports = nextConfig;

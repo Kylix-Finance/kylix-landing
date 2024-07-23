0c9961ce982b2d3e836a36bedbf22a1a29f1f857
Arian Hamdi <arian.hamdi@gmail.com>
2024-07-23 04:48:22 +0330

    add shared package

---
+++ packages/shared/src/constants/queryKeys.ts
@@ -0,0 +1,9 @@
+const baseKey = process.env.NEXT_PUBLIC_REACT_QUERY_BASE_KEY;
+
+export const queryKeys = {
+  accounts: [baseKey, "accounts"],
+  activeAccount: [baseKey, "active-account"],
+  connectionRequest: [baseKey, "connection-request"],
+  config: [baseKey, "config"],
+  status: [baseKey, "status"],
+};
---
+++ packages/shared/src/types/account.ts
@@ -0,0 +1,5 @@
+import { InjectedAccount } from "@polkadot/extension-inject/types";
+
+export type Accounts = {
+  accounts: InjectedAccount[];
+};

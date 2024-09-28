fab7d014335ff303eaecd4435b9293a2249f707b
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-09-28 19:00:57 +0330

    feat: add gradient to card

--- apps/landing/src/app/_parts/SmartLending/components/Card/index.tsx
+++ apps/landing/src/app/_parts/SmartLending/components/Card/index.tsx
@@ -1,13 +1,27 @@
 import Link from "next/link";
 import { ArrowRight } from "~/assets/svgs";
 import { SmartLending } from "~/types";
+
 const Card = ({ to, description, heading }: SmartLending) => {
   return (
     <Link
       href={to}
-      className="relative w-full h-72 lg:h-[411px] rounded-3xl p-7 bg-secondary-500 overflow-hidden"
+      className="relative w-full h-72 lg:h-[411px] rounded-3xl p-7 overflow-hidden bg-secondary-500"
     >
-      <div className="flex flex-col justify-between text-white w-full h-full">
+      <div
+        className="absolute inset-0 rounded-3xl"
+        style={{
+          background: `radial-gradient(circle at bottom, #56DDB490 0%, rgba(17, 23, 61, 0) 80%)`,
+        }}
+      />
+      <div
+        className="absolute inset-0 rounded-3xl"
+        style={{
+          background: `radial-gradient(circle at bottom right, #56DDB490 0%, rgba(17, 23, 61, 0) 60%)`,
+        }}
+      />
+
+      <div className="flex flex-col justify-between text-white w-full h-full relative z-10">
         <p className="text-sm lg:text-base font-light leading-6">
           {description}
         </p>
@@ -18,8 +32,6 @@ const Card = ({ to, description, heading }: SmartLending) => {
           <ArrowRight />
         </div>
       </div>
-      {/* <div className="absolute w-full h-full smartLandingItem  -right-[70%] top-[40%]" />
-            <div className="absolute w-full h-full smartLandingItem  -right-[30%] top-[70%]" /> */}
     </Link>
   );
 };

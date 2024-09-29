7c3d2f6b54d65669f7edd9b9f99ca7cbd3cfe8ef
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-09-29 20:44:20 +0330

    fix: styles

--- apps/landing/src/app/_parts/SecurityAndAudits/components/Card.tsx
+++ apps/landing/src/app/_parts/SecurityAndAudits/components/Card.tsx
@@ -2,7 +2,7 @@ import { Security } from "~/types";
 
 const Card = ({ description, heading, icon: Icon }: Security) => {
   return (
-    <div className="flex flex-col gap-5 justify-center items-center md:justify-start md:items-center">
+    <div className="flex flex-col gap-5 justify-center items-center md:justify-start md:items-start">
       <div className="relative rounded-xl bg-gradient-to-br from-white/20 via-transparent p-0.5 w-24 h-24">
         <div
           className="absolute inset-0 rounded-xl"
--- apps/landing/src/app/page.tsx
+++ apps/landing/src/app/page.tsx
@@ -1,11 +1,14 @@
-import Section from "~/components/Section";
-import Hero from "./_parts/Hero";
-import SmartLending from "./_parts/SmartLending";
-import MarketTrends from "./_parts/MarketTrends";
-import SupportedAssetsAndAPY from "./_parts/SupportedAssetsAndAPY";
-import HowItWorks from "./_parts/HowItWorks";
-import SecurityAndAudits from "./_parts/SecurityAndAudits";
-import Footer from "./_parts/Footer";
+import dynamic from "next/dynamic";
+
+const Hero = dynamic(() => import("./_parts/Hero"));
+const SmartLending = dynamic(() => import("./_parts/SmartLending"));
+const MarketTrends = dynamic(() => import("./_parts/MarketTrends"));
+const SupportedAssetsAndAPY = dynamic(
+  () => import("./_parts/SupportedAssetsAndAPY")
+);
+const HowItWorks = dynamic(() => import("./_parts/HowItWorks"));
+const SecurityAndAudits = dynamic(() => import("./_parts/SecurityAndAudits"));
+const Footer = dynamic(() => import("./_parts/Footer"));
 
 export default function page() {
   return (

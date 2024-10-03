845cf02ffe2608e4dd33c659ff7a480ac255f2a3
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-10-03 23:09:30 +0330

    fix: header conraienr

--- apps/landing/src/app/_parts/Header/index.tsx
+++ apps/landing/src/app/_parts/Header/index.tsx
@@ -4,8 +4,8 @@ import { Nav } from "./Nav";
 
 const StickyHeader = () => {
   return (
-    <header className="sticky top-0 z-50 text-white py-[16px]">
-      <div className="container mx-auto py-[16px] px-[33px] flex justify-between items-center rounded-[16px] border border-[#20332D] bg-gradient-to-r from-[#11121439] to-[#0C0D0F45]">
+    <header className="sticky top-0 z-50 text-white py-[16px] ">
+      <div className="container mx-auto py-[16px] px-[33px] flex justify-between items-center rounded-[16px] border border-[#20332D] bg-gradient-to-r from-[#11121439] to-[#0C0D0F45] backdrop-blur-sm">
         <div className="font-bold text-lg">
           <span className="tracking-wide">
             <Kylix />

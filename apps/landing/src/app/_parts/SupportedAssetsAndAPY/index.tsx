e4a90a0f2c66a34bc9de5029aa3d89ea5723a05c
Arian Hamdi <arian.hamdi@gmail.com>
2024-10-04 07:34:56 +0330

    fix: supported assets responsive

--- apps/landing/src/app/_parts/SupportedAssetsAndAPY/index.tsx
+++ apps/landing/src/app/_parts/SupportedAssetsAndAPY/index.tsx
@@ -6,16 +6,15 @@ const SupportedAssetsAndAPY = () => {
     <Section
       heading={{ left: "Supported ", right: " Assets and APY" }}
       description="The new cross-chain Lending Platform, on Polkadot."
-      id="supported-assets-and-apy"
     >
       <div>
-        <div className="flex flex-col lg:flex-row items-center justify-between gap-6">
+        <div className="flex lg:flex-row items-center justify-between gap-6 flex-wrap">
           {assets.map((asset, index) => (
             <div
               key={index}
-              className="p-0.5 bg-gradient-to-br from-white/20 via-transparent rounded-md"
+              className="p-0.5 bg-gradient-to-br from-white/20 via-transparent rounded-md w-[calc(50%-0.75rem)] xl:w-auto"
             >
-              <div className="p-9 rounded-md backdrop-blur-3xl  bg-gradient-to-tl from-white/5 to-transparent">
+              <div className="flex items-center justify-center p-9 rounded-md backdrop-blur-3xl bg-gradient-to-tl from-white/5 to-transparent aspect-square">
                 <Image
                   src={asset.src}
                   alt={asset.alt}

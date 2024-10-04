cb6a514e897d45b003ae03ef2003fc51a382faf5
Arian Hamdi <arian.hamdi@gmail.com>
2024-10-04 09:47:23 +0330

    fix: improve responsive

--- apps/landing/src/app/_parts/SupportedAssetsAndAPY/index.tsx
+++ apps/landing/src/app/_parts/SupportedAssetsAndAPY/index.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Section from "~/components/Section";
 import { assets } from "~/data";
+
 const SupportedAssetsAndAPY = () => {
   return (
     <Section
@@ -9,11 +10,11 @@ const SupportedAssetsAndAPY = () => {
       id="supported-assets-and-apy"
     >
       <div>
-        <div className="flex lg:flex-row items-center justify-between gap-6 flex-wrap">
+        <div className="grid grid-cols-2 gap-6 md:grid-cols-4">
           {assets.map((asset, index) => (
             <div
               key={index}
-              className="p-0.5 bg-gradient-to-br from-white/20 via-transparent rounded-md w-[calc(50%-0.75rem)] xl:w-auto"
+              className="p-0.5 bg-gradient-to-br from-white/20 via-transparent rounded-md"
             >
               <div className="flex items-center justify-center p-9 rounded-md backdrop-blur-3xl bg-gradient-to-tl from-white/5 to-transparent aspect-square">
                 <Image

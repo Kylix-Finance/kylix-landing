59f820edad961518f1843a3260f92673b90b2f53
Arian Hamdi <arian.hamdi@gmail.com>
2024-10-04 08:13:59 +0330

    fix: improve animation

--- apps/landing/src/app/_parts/MarketTrends/components/Card.tsx
+++ apps/landing/src/app/_parts/MarketTrends/components/Card.tsx
@@ -3,54 +3,69 @@ import Button from "~/components/Button";
 import { marketTrends } from "~/data";
 import { motion, Variants } from "framer-motion";
 const variants: Variants = {
-    hidden: { opacity: 1, y: 30 },
-    visible: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: -30 },
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: -20 },
 };
 
 interface Props {
-    id: number;
+  id: number;
 }
 
 const Card = ({ id }: Props) => {
-    const currentSlide = marketTrends.find((item) => item.id === id);
-    if (!currentSlide) return null;
+  const currentSlide = marketTrends.find((item) => item.id === id);
+  if (!currentSlide) return null;
 
-
-
-    return (
-        <motion.div
-            className="flex flex-col md:flex-row gap-16 items-center justify-between"
-            key={currentSlide.id}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.5 }}
-        >
-            <div className="flex flex-col gap-8 order-2 md:order-1">
-                <div className="flex flex-col gap-8">
-                    {currentSlide.items.map((item, index) => (
-                        <div className="flex flex-col gap-1" key={index}>
-                            <h3 className="font-bold text-2xl leading-8 text-white">{item.title}</h3>
-                            <p className="font-normal text-base leading-6 text-secondary-300">{item.description}</p>
-                        </div>
-                    ))}
-                </div>
-                <div className="flex items-center gap-2.5">
-                    <Button onClick={currentSlide.actions.primary.action} variant="primary" color="secondary">
-                        {currentSlide.actions.primary.label}
-                    </Button>
-                    <Button onClick={currentSlide.actions.secondary.action} variant="outline" color="white">
-                        {currentSlide.actions.secondary.label}
-                    </Button>
-                </div>
+  return (
+    <motion.div
+      className="flex flex-col md:flex-row gap-16 items-center justify-between"
+      key={currentSlide.id}
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      variants={variants}
+      transition={{ duration: 0.3 }}
+    >
+      <div className="flex flex-col gap-8 order-2 md:order-1">
+        <div className="flex flex-col gap-8">
+          {currentSlide.items.map((item, index) => (
+            <div className="flex flex-col gap-1" key={index}>
+              <h3 className="font-bold text-2xl leading-8 text-white">
+                {item.title}
+              </h3>
+              <p className="font-normal text-base leading-6 text-secondary-300">
+                {item.description}
+              </p>
             </div>
-            <div className="relative w-full h-96 order-1">
-                <Image fill src={currentSlide.image} alt={currentSlide.label} />
-            </div>
-        </motion.div>
-    );
+          ))}
+        </div>
+        <div className="flex items-center gap-2.5">
+          <Button
+            onClick={currentSlide.actions.primary.action}
+            variant="primary"
+            color="secondary"
+          >
+            {currentSlide.actions.primary.label}
+          </Button>
+          <Button
+            onClick={currentSlide.actions.secondary.action}
+            variant="outline"
+            color="white"
+          >
+            {currentSlide.actions.secondary.label}
+          </Button>
+        </div>
+      </div>
+      <div className="relative w-full order-1">
+        <Image
+          width={644}
+          height={384}
+          src={currentSlide.image}
+          alt={currentSlide.label}
+        />
+      </div>
+    </motion.div>
+  );
 };
 
 export default Card;
--- apps/landing/src/app/_parts/MarketTrends/index.tsx
+++ apps/landing/src/app/_parts/MarketTrends/index.tsx
@@ -18,13 +18,13 @@ const MarketTrends = () => {
       id="markets-trends"
     >
       <div className="flex flex-col pt-4 pb-8 px-8 gap-12 rounded-md border border-secondary-400 w-full shadow-[0px 64px 64px -48px rgba(15, 15, 15, 0.1)] bg-[#DBC5F510]">
-        <div className="flex justify-center items-center gap-7 border-b-[3px] border-[#23262F]">
+        <div className="flex justify-center border-b-[3px] border-[#23262F] overflow-x-scroll overflow-y-hidden hide-scrollbar">
           {marketTrends.map((item) => (
-            <div key={item.id} className="relative pb-0.5">
+            <div key={item.id} className="relative pb-0.5 shrink-0">
               <p
                 onClick={() => setCurrentSlide(item.id)}
                 className={clsx(
-                  "text-sm  leading-4 py-2 px-4  cursor-pointer",
+                  "text-sm leading-4 py-2 px-4 cursor-pointer transition-all whitespace-nowrap",
                   currentSlide === item.id
                     ? "text-white font-bold"
                     : "text-secondary-100 font-normal"
@@ -32,28 +32,18 @@ const MarketTrends = () => {
               >
                 {item.label}
               </p>
-              <AnimatePresence>
-                {item.id === currentSlide && (
-                  <motion.div
-                    layout
-                    layoutId="underline"
-                    initial={{ opacity: 0, x: -20 }}
-                    animate={{ opacity: 1, x: 0 }}
-                    exit={{ opacity: 0, x: 20 }}
-                    transition={{
-                      duration: 0.5,
-                      ease: "easeInOut",
-                      type: "tween",
-                    }}
-                    className="w-full h-1 bg-primary-500 absolute rounded-full"
-                  />
-                )}
-              </AnimatePresence>
+              {item.id === currentSlide && (
+                <motion.div
+                  layout
+                  layoutId="underline"
+                  className="w-full h-1 bg-primary-500 absolute rounded-full"
+                />
+              )}
             </div>
           ))}
         </div>
         <AnimatePresence mode="wait">
-          <Card id={currentSlide} />
+          <Card key={currentSlide} id={currentSlide} />
         </AnimatePresence>
       </div>
     </Section>

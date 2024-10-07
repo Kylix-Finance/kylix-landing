779221a93307a1025164e79c4e8f56ca2cc02ef0
dev-aly3n <aly3n.dev@gmail.com>
2024-10-07 11:12:35 +0330

    feat: add hero

---
+++ apps/landing/src/app/_parts/Hero/components/Jar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { useGLTF } from "@react-three/drei";
+import { motion } from "framer-motion-3d";
+import * as THREE from "three";
+import { MotionValue, useSpring, useTransform } from "framer-motion";
+interface Props {
+  scrollYProgress: MotionValue<number>;
+}
+const Jar = ({ scrollYProgress }: Props) => {
+  const { nodes } = useGLTF("/jar.glb");
+
+  const rotation = useTransform(scrollYProgress, [0, 1], [-Math.PI / 2.3, 0]);
+  const sRotation = useSpring(rotation, {
+    damping: 60,
+    stiffness: 1000,
+    bounce: 0,
+    mass: 1,
+  });
+  const position = useTransform(scrollYProgress, [0, 1], [0.2, -0.5]);
+  const sPosition = useSpring(position, {
+    damping: 60,
+    stiffness: 1000,
+    bounce: 0,
+    mass: 1,
+  });
+
+  return (
+    <motion.group
+      scale={[1.25, 1.25, 1.25]}
+      dispose={null}
+      rotation-x={sRotation}
+      rotation-y={-0.04}
+      position-y={sPosition}
+    >
+      <motion.group name="Mesh_0">
+        {Object.entries(nodes).map(([name, node]) => {
+          if (node instanceof THREE.Mesh) {
+            const material = node.material as THREE.MeshStandardMaterial;
+
+            return (
+              <motion.mesh key={name} name={name} geometry={node.geometry}>
+                <meshStandardMaterial
+                  color={material.color}
+                  emissive={material.emissive}
+                  emissiveIntensity={material.emissiveIntensity}
+                  emissiveMap={material.emissiveMap}
+                  metalness={1.5}
+                  roughness={0.1}
+                  transparent={material.transparent}
+                  opacity={material.opacity}
+                />
+              </motion.mesh>
+            );
+          }
+          return null;
+        })}
+      </motion.group>
+    </motion.group>
+  );
+};
+
+export default Jar;
+
+useGLTF.preload("/jar.glb");
---
+++ apps/landing/src/app/_parts/Hero/components/Scene.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Environment, OrbitControls } from "@react-three/drei";
+import Jar from "./Jar";
+import { Suspense, useEffect, useRef } from "react";
+import { EffectComposer, Bloom } from "@react-three/postprocessing";
+import { MotionCanvas, LayoutCamera } from "framer-motion-3d";
+import { MotionConfig, MotionValue, useTransform } from "framer-motion";
+
+import * as THREE from "three";
+import { extend } from "@react-three/fiber";
+
+extend(THREE);
+
+interface Props {
+  scrollYProgress: MotionValue<number>;
+}
+
+const Scene = ({ scrollYProgress }: Props) => {
+  return (
+    <MotionConfig>
+      <MotionCanvas shadows dpr={[1, 2]} style={{ height: "100vh" }}>
+        <LayoutCamera makeDefault position={[0, 0.6, -2.5]} fov={85} />
+        <OrbitControls
+          enablePan={false}
+          enableZoom={false}
+          enableRotate={false}
+          enableDamping={true}
+          dampingFactor={0.01}
+        />
+        <Suspense fallback={null}>
+          <EffectComposer>
+            <Bloom intensity={0.2} width={1000} height={1000} kernelSize={5} />
+          </EffectComposer>
+          <Jar scrollYProgress={scrollYProgress} />
+          <Environment preset="warehouse" />
+        </Suspense>
+      </MotionCanvas>
+    </MotionConfig>
+  );
+};
+
+export default Scene;

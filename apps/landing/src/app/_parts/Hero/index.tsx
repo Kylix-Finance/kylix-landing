d413c9c5cebc7cbd0a8fa1fdf0c588ec277a8988
dev-aly3n <aly3n.dev@gmail.com>
2024-10-11 15:31:38 +0330

    feat: add second text of landing

--- apps/landing/src/app/_parts/Hero/index.tsx
+++ apps/landing/src/app/_parts/Hero/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useSpring } from "framer-motion";
 import Scene from "./components/Scene";
 import { useRef } from "react";
 import { useScroll, useTransform } from "framer-motion";
@@ -36,6 +36,23 @@ export default function Home() {
     [0, 0.3, 0.45, 1],
     [0, -90, -90, -90]
   );
+  const secondTextOpacity = useTransform(
+    scrollYProgress,
+    [0, 0.45, 0.55, 1],
+    [0, 0, 1, 1]
+  );
+  const secondTextTranslateY = useTransform(
+    scrollYProgress,
+    [0, 0.45, 0.5, 1],
+    [0, 0, 0, -200]
+  );
+
+  const sSecondTextTranslateY = useSpring(secondTextTranslateY, {
+    damping: 60,
+    stiffness: 1000,
+    bounce: 0,
+    mass: 1,
+  });
 
   return (
     <div className="flex flex-col w-full h-full " ref={scrollContainerRef}>
@@ -77,6 +94,29 @@ export default function Home() {
             </div>
           </motion.div>
           {/* FIRST Text END */}
+
+          {/* ---------------------- */}
+
+          {/* SECOND Text START */}
+          <motion.div
+            style={{
+              opacity: secondTextOpacity,
+              translateY: sSecondTextTranslateY,
+            }}
+            className="absolute top-[35%] flex flex-col gap-2 justify-center items-center z-50"
+          >
+            <h2 className="flex flex-col sm:flex-row justify-center items-center gap-2.5 font-bold font-heading w-full h-full text-4xl md:text-5xl lg:text-6xl">
+              <span className="text-primary-500">Multi-chain</span>
+              <span className="text-white"> Lending</span>
+            </h2>
+            <p className="font-light text-xs md:text-sm lg:text-base leading-6 text-secondary-100 text-center ">
+              The new cross-chain Lending Platform, on Polkadot.
+            </p>
+            <div className="flex gap-4">
+              <Button color="secondary">Get start</Button>
+            </div>
+          </motion.div>
+          {/* SECOND Text END */}
         </motion.div>
       </div>
     </div>

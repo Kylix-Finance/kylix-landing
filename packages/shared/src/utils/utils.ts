54628b2a880a6cc6f90837aaab6de9dca091b566
dev-aly3n <aly3n.dev@gmail.com>
2024-10-27 22:35:22 +0330

    fix: repay functionality

--- packages/shared/src/utils/utils.ts
+++ packages/shared/src/utils/utils.ts
@@ -3,3 +3,5 @@ export const getWalletExtension = (walletId: string) =>
 
 export const decodeArrayToString = (arr: number[]) =>
   String.fromCharCode(...arr);
+
+export const minBigInt = (a: bigint, b: bigint): bigint => (a < b ? a : b);

70e7c52eb8c1ae6aba1a418529797b325ffdae8a
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2024-11-02 22:12:58 +0330

    fix: position of content page

--- apps/landing/src/app/[slug]/page.tsx
+++ apps/landing/src/app/[slug]/page.tsx
@@ -28,7 +28,7 @@ export default async function page({ params }: { params: { slug: string } }) {
   }
 
   return (
-    <article className="py-8 px-4 text-white mt-16 flex flex-col items-center sm:py-12 md:py-16 lg:py-20">
+    <article className="py-8 px-4 text-white mt-16 flex flex-col items-center sm:py-12 md:py-16 lg:py-20 z-20 relative">
       <h1 className="text-4xl sm:text-5xl md:text-6xl font-heading font-bold mb-4 text-primary-500 text-center">
         {post.title}
       </h1>

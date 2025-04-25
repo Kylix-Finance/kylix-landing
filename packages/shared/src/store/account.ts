0db4bcb673924ee11b2a8432f7f81d2196ef1d22
Mohammadreza Alizadeh <mohrezalizadeh@gmail.com>
2025-04-25 12:19:39 +0400

    feat: add new option to preference

--- packages/shared/src/store/account.ts
+++ packages/shared/src/store/account.ts
@@ -1,18 +1,25 @@
 import { InjectedAccount } from "@polkadot/extension-inject/types";
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+type Stage = "walletsList" | "accountsList" | "switchAccount" | null;
 
 type UseAccountsStoreSchema = {
   account: InjectedAccount | null;
   connectorId: string | null;
   accounts?: InjectedAccount[];
+  stage: Stage;
 };
 
 type UseAccountsStore = {
   setAccount: (account: InjectedAccount) => void;
   setConnectorId: (connectorId: string) => void;
-  connect: (connectorId: string, accounts: InjectedAccount[]) => void;
-  disconnect: () => void;
+  connect: (
+    connectorId: string,
+    accounts: InjectedAccount[],
+    stage?: Stage
+  ) => void;
+  disconnect: (stage?: Stage) => void;
+  setStage: (stage: Stage) => void;
 } & UseAccountsStoreSchema;
 
 export const useAccountsStore = create<UseAccountsStore>()(
@@ -21,6 +28,7 @@ export const useAccountsStore = create<UseAccountsStore>()(
       account: null,
       connectorId: null,
       accounts: undefined,
+      stage: null,
       setAccount: (account: InjectedAccount) =>
         set({
           account,
@@ -29,19 +37,31 @@ export const useAccountsStore = create<UseAccountsStore>()(
         set({
           connectorId,
         }),
-      connect: (connectorId: string, accounts: InjectedAccount[]) =>
+      connect: (
+        connectorId: string,
+        accounts: InjectedAccount[],
+        stage: Stage = null
+      ) =>
         set({
           connectorId,
           accounts,
+          account: accounts[0],
+          stage,
         }),
-      disconnect: () =>
+      disconnect: (stage: Stage = null) =>
         set({
           account: null,
           connectorId: null,
+          stage,
+        }),
+      setStage: (stage: Stage) =>
+        set({
+          stage,
         }),
     }),
     {
       name: "accounts",
+      partialize: ({ stage, ...rest }) => rest,
     }
   )
 );
